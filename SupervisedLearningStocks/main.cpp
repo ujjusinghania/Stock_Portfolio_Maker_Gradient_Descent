ce62a31435e354ba2b4da414c9cf8a667cc47551
Ujjwal Singhania <ujjwalsinghania@gmail.com>
2017-06-08 17:42:45 +0530

    Added ability to read from .csv files.
    - Added the ability to read from .csv files.
    - Added provision for the program to ignore BOM (Byte Order Mark) if
    present.
    - Added test code to test if gradient descent is working.

--- SupervisedLearningStocks/main.cpp
+++ SupervisedLearningStocks/main.cpp
@@ -6,11 +6,65 @@
 //  Copyright © 2017 Ujjwal Singhania. All rights reserved.
 //
 
-#include "DataMatrix.hpp"
+#include "Hypothesis.hpp"
 #include <iostream>
+#include <fstream>
+#include <algorithm>
+#include <sstream>
+using namespace std;
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+vector<TrainingExample> readDataFromFile(const string& fileName) {
+    ifstream fileInput (fileName);
+    
+    if (!fileInput) {
+        cout << "Data from the file could not be read." << endl;
+        exit(1);
+    }
+    
+    vector<TrainingExample> hypothesisData;
+    vector<double> dataValues;
+    string value = "";
+    
+    // To check for Byte Order Mark (BOM).
+    
+    char bomCheck[4] = {0};
+    fileInput.read(bomCheck, 3);
+    if (strcmp(bomCheck, "\xEF\xBB\xBF") != 0) {
+        fileInput.seekg(0);
+    }
+    
+    while (fileInput >> value) {
+        value = value + ",end,";
+        string tableValue;
+        stringstream ss = stringstream(value);
+        while (getline(ss, tableValue, ',')) {
+            if (tableValue == "end") {
+                double target = dataValues.back();
+                dataValues.pop_back();
+                TrainingExample dataExample = TrainingExample(dataValues, target);
+                hypothesisData.push_back(dataExample);
+                dataValues = {};
+                continue;
+            }
+            
+            try {
+                dataValues.push_back(stod(tableValue));
+            }
+            catch (invalid_argument) {
+                cout << "Invalid";
+                continue;
+            }
+        }
+    }
+    return hypothesisData;
+}
+
+int main() {
+    vector<TrainingExample> trainingExamples = readDataFromFile("mockData.csv");
+    Hypothesis companyA = Hypothesis(trainingExamples);
+    cout << endl << endl << companyA << endl << endl;
+    vector<double> theta = companyA.performGradientDescent();
+    for (size_t thetaIndex = 0; thetaIndex < theta.size(); thetaIndex++) {
+        cout << "theta[" << thetaIndex << "] = " << theta[thetaIndex] << endl;
+    }
 }
